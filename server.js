45a428e0e05bfc629bb4d939a085a24aa2dbdc65
Chandrasekhar Samanta <chandrasekhar.2039@gmail.com>
2020-12-14 19:42:26 +0530

    testing api-undone-2

--- server.js
+++ server.js
@@ -2,36 +2,33 @@ const express=require("express");
 const CloudmersiveConvertApiClient = require('cloudmersive-convert-api-client');
 var fs = require('fs');
 const app=express();
+const path = require('path');
 
 
 var defaultClient = CloudmersiveConvertApiClient.ApiClient.instance;
 
-// Configure API key authorization: Apikey
-var Apikey = defaultClient.authentications['Apikey'];
-Apikey.apiKey = "b45e0b32-6c44-404e-90db-0cdd88b7e570"
-
-var api = new CloudmersiveConvertApiClient.ConvertDataApi()
-
 app.get("/input",function (req,res){
+  // Configure API key authorization: Apikey
+var Apikey = defaultClient.authentications['Apikey'];
+Apikey.apiKey = 'b45e0b32-6c44-404e-90db-0cdd88b7e570';
+
+
 
 var apiInstance = new CloudmersiveConvertApiClient.ConvertDocumentApi();
 
-// var inputFile = fs.readFileSync("test.docx"); // {File} Input file to perform the operation on.
-var inputFile =__dirname+"/test.docx"; // File | Input file to perform the operation on.
+var inputFile = Buffer.from(fs.readFileSync("./test.docx").buffer); // File | Input file to perform the operation on.
 
+const loc=path.resolve(__dirname,'client', 'testt.pdf');
 
 var callback = function(error, data, response) {
-  console.log("working");
   if (error) {
     console.error(error);
-    console.log("error");
   } else {
     console.log('API called successfully. Returned data: ');
+    response.pipe(fs.createWriteStream(loc));
   }
 };
-// api.convertDataCsvToJson(Buffer.from(inputFile.buffer), callback);
 apiInstance.convertDocumentAutodetectToPdf(inputFile, callback);
-res.end();
 });
 
 
